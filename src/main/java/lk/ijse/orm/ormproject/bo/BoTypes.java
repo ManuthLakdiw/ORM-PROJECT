9af331a22c73587e709b3d80f959a4a3e31277c0
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-03-13 20:13:04 +0530

    added Bo types

---
+++ src/main/java/lk/ijse/orm/ormproject/bo/BoTypes.java
@@ -0,0 +1,11 @@
+package lk.ijse.orm.ormproject.bo;
+
+/**
+ * @author manuthlakdiv
+ * @email manuthlakdiv2006.com
+ * @project ORM-PROJECT
+ * @github https://github.com/ManuthLakdiw
+ */
+public enum BoTypes {
+    USER
+}
