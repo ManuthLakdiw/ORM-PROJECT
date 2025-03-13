01a4236e596da271002852fa7aef59c597d46250
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-03-13 20:13:45 +0530

    added User Entity

---
+++ src/main/java/lk/ijse/orm/ormproject/entity/User.java
@@ -0,0 +1,10 @@
+package lk.ijse.orm.ormproject.entity;
+
+/**
+ * @author manuthlakdiv
+ * @email manuthlakdiv2006.com
+ * @project ORM-PROJECT
+ * @github https://github.com/ManuthLakdiw
+ */
+public class User {
+}
