f00d0730c092d83000030cc288df6d3f818cfb03
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-03-13 21:53:59 +0530

    created Dao Factory

---
+++ src/main/java/lk/ijse/orm/ormproject/dao/DaoFactory.java
@@ -0,0 +1,24 @@
+package lk.ijse.orm.ormproject.dao;
+
+import lk.ijse.orm.ormproject.dao.custom.impl.UserDaoImpl;
+
+/**
+ * @author manuthlakdiv
+ * @email manuthlakdiv2006.com
+ * @project ORM-PROJECT
+ * @github https://github.com/ManuthLakdiw
+ */
+public class DaoFactory {
+    private static DaoFactory daoFactory;
+    private DaoFactory() {}
+
+    public static DaoFactory getInstance() {
+        return daoFactory == null ? daoFactory = new DaoFactory() : daoFactory;
+    }
+
+    public <T extends SuperDao> T getDao(DaoTypes daoType) {
+        return switch (daoType){
+            case USER -> (T) new UserDaoImpl();
+        };
+    }
+}
