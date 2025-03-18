8061c79a7ce7a9b4f43da319f5c9a544100b5dd4
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-03-19 02:06:35 +0530

    created exception for username duplicate

---
+++ src/main/java/lk/ijse/orm/ormproject/bo/exception/UserNameDuplicateException.java
@@ -0,0 +1,14 @@
+package lk.ijse.orm.ormproject.bo.exception;
+
+/**
+ * @author manuthlakdiv
+ * @email manuthlakdiv2006.com
+ * @project ORM-PROJECT
+ * @github https://github.com/ManuthLakdiw
+ */
+public class UserNameDuplicateException extends Exception {
+
+    public UserNameDuplicateException(String message) {
+        super(message);
+    }
+}
