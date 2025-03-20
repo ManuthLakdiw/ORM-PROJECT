af667730e9636c40a8c8eaa37074080d223fc13b
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-03-20 11:27:09 +0530

    added user business logic methods

--- src/main/java/lk/ijse/orm/ormproject/bo/custom/UserBo.java
+++ src/main/java/lk/ijse/orm/ormproject/bo/custom/UserBo.java
@@ -1,6 +1,11 @@
 package lk.ijse.orm.ormproject.bo.custom;
 
 import lk.ijse.orm.ormproject.bo.SuperBo;
+import lk.ijse.orm.ormproject.bo.exception.UserNameDuplicateException;
+import lk.ijse.orm.ormproject.dto.UserDto;
+import lk.ijse.orm.ormproject.entity.User;
+
+import java.util.List;
 
 /**
  * @author manuthlakdiv
@@ -9,5 +14,15 @@ import lk.ijse.orm.ormproject.bo.SuperBo;
  * @github https://github.com/ManuthLakdiw
  */
 public interface UserBo extends SuperBo {
-    boolean verifyUser(String username, String password);
+
+    boolean saveUser(UserDto userDto) throws Exception;
+    List<UserDto> getAllUsers() throws Exception;
+    boolean verifyUser(String username, String password) throws Exception;
+    String generateNewID() throws Exception;
+    boolean validUserName(String username) throws Exception;
+    boolean deleteUser(String id) throws Exception;
+    boolean updateUser(UserDto userDto) throws Exception;
+    String getUserNameById(String id) throws Exception;
+
+
 }
