58a28ffcb1831a1e3d32719e0f988ed4503551b5
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-03-20 11:29:59 +0530

    added unique method

--- src/main/java/lk/ijse/orm/ormproject/dao/custom/UserDao.java
+++ src/main/java/lk/ijse/orm/ormproject/dao/custom/UserDao.java
@@ -1,10 +1,18 @@
 package lk.ijse.orm.ormproject.dao.custom;
 
+import lk.ijse.orm.ormproject.dao.CrudDao;
+import lk.ijse.orm.ormproject.entity.User;
+
+import java.util.Optional;
+
 /**
  * @author manuthlakdiv
  * @email manuthlakdiv2006.com
  * @project ORM-PROJECT
  * @github https://github.com/ManuthLakdiw
  */
-public interface UserDao {
+public interface UserDao extends CrudDao<User, String> {
+
+    User getUserByUserName(String username) throws Exception;
+
 }
