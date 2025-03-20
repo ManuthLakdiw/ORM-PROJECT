946ffdb5a0dce5bf7ba7b857eab1b2161f4021a0
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-03-20 11:32:32 +0530

    updated module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,17 +1,22 @@
 module lk.ijse.orm.ormproject {
 
-    requires javafx.controls;
     requires javafx.fxml;
     requires lombok;
     requires org.hibernate.orm.core;
     requires jakarta.persistence;
     requires java.naming;
     requires jbcrypt;
+    requires java.desktop;
+    requires com.jfoenix;
+    requires javafx.controls;
 
     opens lk.ijse.orm.ormproject.controller to javafx.fxml;
     exports lk.ijse.orm.ormproject;
 
 
     opens lk.ijse.orm.ormproject.config to jakarta.persistence;
+    opens lk.ijse.orm.ormproject.dto.tm to javafx.base;
+    opens lk.ijse.orm.ormproject.dto to javafx.fxml;
+
     opens lk.ijse.orm.ormproject.entity to org.hibernate.orm.core;
 }
