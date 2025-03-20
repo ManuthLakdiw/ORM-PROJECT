ea91b820682528016e88a57b535c493becac59db
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-03-20 11:33:51 +0530

    updated FactoryConfiguration

--- src/main/java/lk/ijse/orm/ormproject/config/FactoryConfiguration.java
+++ src/main/java/lk/ijse/orm/ormproject/config/FactoryConfiguration.java
@@ -1,6 +1,7 @@
 package lk.ijse.orm.ormproject.config;
 
-
+import lk.ijse.orm.ormproject.entity.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -11,39 +12,36 @@ import java.util.Properties;
  * @author manuthlakdiv
  * @email manuthlakdiv2006.com
  * @project ORM-PROJECT
- * @github https://github.com/ManuthLakdiw
+ * @github <a href="https://github.com/ManuthLakdiw">...</a>
  */
-
 
 public class FactoryConfiguration {
     private static FactoryConfiguration factoryConfiguration;
-
-    SessionFactory sessionFactory;
-
+    private final SessionFactory sessionFactory;
     private FactoryConfiguration() {
         Configuration cfg = new Configuration();
-
         Properties properties = new Properties();
 
         try {
             properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("hibernate.properties"));
         } catch (IOException e) {
             e.printStackTrace();
+            throw new RuntimeException("Failed to load hibernate.properties");
         }
 
         cfg.setProperties(properties);
-        cfg.buildSessionFactory();
+        cfg.addAnnotatedClass(User.class);
+        sessionFactory = cfg.buildSessionFactory();
     }
 
     public static FactoryConfiguration getInstance() {
-        return factoryConfiguration == null ? factoryConfiguration = new FactoryConfiguration() : factoryConfiguration;
+        if (factoryConfiguration == null) {
+            factoryConfiguration = new FactoryConfiguration();
+        }
+        return factoryConfiguration;
     }
 
-    public SessionFactory getSessionFactory() {
-        return sessionFactory;
+    public Session getSession() {
+        return sessionFactory.openSession();
     }
-
-
-
-
 }
