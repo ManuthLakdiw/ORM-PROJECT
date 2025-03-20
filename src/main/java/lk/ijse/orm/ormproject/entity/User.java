94d9d5baf40d0f4e00fb3339c96e749124381945
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-03-20 11:25:17 +0530

    define user table

--- src/main/java/lk/ijse/orm/ormproject/entity/User.java
+++ src/main/java/lk/ijse/orm/ormproject/entity/User.java
@@ -1,10 +1,30 @@
 package lk.ijse.orm.ormproject.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * @author manuthlakdiv
  * @email manuthlakdiv2006.com
  * @project ORM-PROJECT
  * @github https://github.com/ManuthLakdiw
  */
-public class User {
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "user_table")
+public class User implements SuperEntity {
+
+    @Id
+    @Column(name = "user_id")
+    private String id;
+    private String username;
+    private String password;
+    private String role;
 }
