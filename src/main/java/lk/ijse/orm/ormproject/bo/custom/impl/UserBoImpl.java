4bbb96b751bf72a98d40565722c18af27591147a
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-03-20 11:27:43 +0530

    implement user business logics

--- src/main/java/lk/ijse/orm/ormproject/bo/custom/impl/UserBoImpl.java
+++ src/main/java/lk/ijse/orm/ormproject/bo/custom/impl/UserBoImpl.java
@@ -1,6 +1,17 @@
 package lk.ijse.orm.ormproject.bo.custom.impl;
 
 import lk.ijse.orm.ormproject.bo.custom.UserBo;
+import lk.ijse.orm.ormproject.bo.exception.UserNameDuplicateException;
+import lk.ijse.orm.ormproject.dao.DaoFactory;
+import lk.ijse.orm.ormproject.dao.DaoTypes;
+import lk.ijse.orm.ormproject.dao.custom.UserDao;
+import lk.ijse.orm.ormproject.dto.UserDto;
+import lk.ijse.orm.ormproject.entity.User;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author manuthlakdiv
@@ -10,8 +21,94 @@ import lk.ijse.orm.ormproject.bo.custom.UserBo;
  */
 public class UserBoImpl implements UserBo {
 
+    UserDao userDao = DaoFactory.getInstance().getDao(DaoTypes.USER);
+
     @Override
-    public boolean verifyUser(String username, String password) {
-        return false;
+    public boolean saveUser(UserDto userDto) throws Exception {
+        String encodedPassword = BCrypt.hashpw(userDto.getPassword(), BCrypt.gensalt());
+        User user = new User();
+        user.setId(userDto.getId());
+        user.setUsername(userDto.getName());
+        user.setPassword(encodedPassword);
+        user.setRole(userDto.getRole());
+        return userDao.save(user);
+
+    }
+
+    @Override
+    public List<UserDto> getAllUsers() throws Exception {
+        List<UserDto> userDtos = new ArrayList<>();
+        List<User> userEntities = userDao.getAll();
+            for (User user : userEntities) {
+                UserDto userDto = new UserDto();
+                userDto.setId(user.getId());
+                userDto.setName(user.getUsername());
+                userDto.setRole(user.getRole());
+                userDtos.add(userDto);
+            }
+            return userDtos;
+    }
+
+    @Override
+    public boolean verifyUser(String username, String password) throws Exception {
+           User user = userDao.getUserByUserName(username);
+           return BCrypt.checkpw(password, user.getPassword());
+    }
+
+    @Override
+    public String generateNewID() throws Exception {
+        Optional<String> lastID = userDao.lastPK();
+        if (lastID.isPresent()) {
+            String substring = lastID.get().substring(1);
+            int number = Integer.parseInt(substring);
+            int newId = number + 1;
+            return String.format("U%03d", newId);
+        } else {
+            return "U001";
+        }
+    }
+
+    public boolean validUserName(String username) throws Exception {
+        boolean notUser = true;
+        List<User> userLists = userDao.getAll();
+
+        for (User user : userLists) {
+            if (user.getUsername().equals(username)) {
+                notUser = false;
+                throw new UserNameDuplicateException("This username is already taken. Please try a different one.");
+            }
+        }
+        return notUser;
+    }
+
+    @Override
+    public boolean deleteUser(String id) throws Exception {
+        return userDao.delete(id);
+    }
+
+    @Override
+    public boolean updateUser(UserDto userDto) throws Exception {
+        User user = new User();
+        user.setId(userDto.getId());
+        user.setUsername(userDto.getName());
+        user.setRole(userDto.getRole());
+        Optional<String> password = Optional.ofNullable(userDto.getPassword());
+        if (password.isPresent()) {
+            String encodePw = BCrypt.hashpw(userDto.getPassword(), BCrypt.gensalt());
+            user.setPassword(encodePw);
+        }
+        return userDao.update(user);
+    }
+
+    @Override
+    public String getUserNameById(String id) throws Exception {
+        List<User> userList = userDao.getAll();
+
+        for (User user : userList) {
+            if (id.equals(user.getId())) {
+                return user.getUsername();
+            }
+        }
+        return null;
     }
 }
