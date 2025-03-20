3e2653ed72c1823c2a2b9380e7b567daa73e8d6e
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-03-20 11:34:08 +0530

    dashboard initial

---
+++ src/main/java/lk/ijse/orm/ormproject/controller/DashBoardFormController.java
@@ -0,0 +1,40 @@
+package lk.ijse.orm.ormproject.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import lk.ijse.orm.ormproject.util.NavigationUtil;
+
+public class DashBoardFormController {
+
+    @FXML
+    private Button btnLogout;
+
+    @FXML
+    private Button btnUser;
+
+    @FXML
+    private AnchorPane dashboardPane;
+
+    @FXML
+    private Pane navPane;
+
+    @FXML
+    void btnLogoutOnMouseClicked(MouseEvent event) {
+        NavigationUtil.getNewStage(
+                (Stage)dashboardPane.getScene().getWindow(),
+                DashBoardFormController.class,
+                "Login",
+                "/view/login.fxml"
+                );
+    }
+
+    @FXML
+    void btnUserOnMouseClicked(MouseEvent event) {
+        NavigationUtil.loadPane(DashBoardFormController.class ,navPane, "user", "/view/user.fxml");
+    }
+
+}
