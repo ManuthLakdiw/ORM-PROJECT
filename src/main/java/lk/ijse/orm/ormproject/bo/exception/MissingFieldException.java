b7770ffaf06d4c4c67775d1023ca64f252966ae1
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-03-20 22:30:03 +0530

    create exception for missing fields

---
+++ src/main/java/lk/ijse/orm/ormproject/bo/exception/MissingFieldException.java
@@ -0,0 +1,13 @@
+package lk.ijse.orm.ormproject.bo.exception;
+
+/**
+ * @author manuthlakdiv
+ * @email manuthlakdiv2006.com
+ * @project ORM-PROJECT
+ * @github https://github.com/ManuthLakdiw
+ */
+public class MissingFieldException extends Exception {
+    public MissingFieldException(String message) {
+        super(message);
+    }
+}
