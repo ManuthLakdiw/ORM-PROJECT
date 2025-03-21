5d29753c86d80f1f690551d64e46eb2e98f54e5d
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-03-21 10:52:26 +0530

    created Programme dto

---
+++ src/main/java/lk/ijse/orm/ormproject/dto/ProgrammeDto.java
@@ -0,0 +1,28 @@
+package lk.ijse.orm.ormproject.dto;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+/**
+ * @author manuthlakdiv
+ * @email manuthlakdiv2006.com
+ * @project ORM-PROJECT
+ * @github https://github.com/ManuthLakdiw
+ */
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProgrammeDto {
+
+    private String id;
+    private String programmeName;
+    private String duration;
+    private BigDecimal fee;
+}
